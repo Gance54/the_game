403ff65a546696cd7cf868d0c6526b6144c79241
Mykhailo Sopiha <gance54@gmail.com>
2019-08-03 22:08:22 +0300

    Cleaning
    remove unsused code
    
    Signed-off-by: Mykhailo Sopiha <gance54@gmail.com>

--- client/src/main.cpp
+++ client/src/main.cpp
@@ -3,40 +3,11 @@
 #include <QDebug>
 #include "login.h"
 
-#include <QtQuick>
-#include <QApplication>
-
-/*int main(int argc, char *argv[])
-{
-    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
-    Login login;
-
-    qmlRegisterType<Login>("io.qt.services.login", 1, 1, "LogInClass");
-
-    QGuiApplication app(argc, argv);
-
-    QQmlApplicationEngine engine;
-    const QUrl url(QStringLiteral("qrc:/qml/login.qml"));
-
-    QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
-                     &app, [url](QObject *obj, const QUrl &objUrl) {
-        if (!obj && url == objUrl)
-            QCoreApplication::exit(-1);
-    }, Qt::QueuedConnection);
-
-
-    engine.load(url);
-
-    QObject::connect(&engine, SIGNAL(qmlSignalPressed(QString, QString)),
-                         &login, SLOT(cppSlot(QString, QString)));
-
-    return app.exec();
-}*/
-
 int main(int argc, char *argv[]) {
-    QApplication app(argc, argv);
+    QGuiApplication app(argc, argv);
     Login login;
 
+    /* Class registration fron login.h */
     qmlRegisterType<Login>("io.qt.services.login", 1, 1, "LogInClass");
 
 
@@ -49,9 +20,12 @@ int main(int argc, char *argv[]) {
         QObject *rootObject = engine.rootObjects().first();
         QObject *qmlObject = rootObject->findChild<QObject*>("LoginButtonObject");
 
-        if(qmlObject)
+        if(qmlObject) {
             QObject::connect(qmlObject, SIGNAL(qmlSignalPressed(QString, QString)),
                                  &login, SLOT(onButtonClicked(QString, QString)));
+
+        }
+
     });
 
     engine.load(url);
